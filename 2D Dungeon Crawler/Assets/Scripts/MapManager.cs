b6011d5eb2a36e0a99a07a5edb483f21e5f905a5
omg3011 <joeljoel3011@gmail.com>
2019-12-19 22:12:34 +0800

    Finished Heatmap Generation

--- 2D Dungeon Crawler/Assets/Scripts/MapManager.cs
+++ 2D Dungeon Crawler/Assets/Scripts/MapManager.cs
@@ -4,195 +4,273 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    // Twerkable
-    public Vector2 _tileSize;
-    public Vector2 _noOfTile;
+  // Twerkable
+  public Vector2 _tileSize;
+  public Vector2 _noOfTile;
 
-    // Reference
-    public GameObject Prefab_Tile;
+  // Reference
+  public GameObject Prefab_Tile;
 
-    // Private
-    private List<Tile> _listOfTile = new List<Tile>();
-    private Tile _tileStart, _tileGoal;
+  // Private
+  private List<Tile> _listOfTile = new List<Tile>();
+  private Tile _tileStart, _tileGoal;
 
 
-    //--------------------------------------------------------------
-    // Unity Function(s)
-    //--------------------------------------------------------------
-    private void Awake()
+  private List<Tile> _openList = new List<Tile>();
+  private List<Tile> _visitedList = new List<Tile>();
+
+
+  //--------------------------------------------------------------
+  // Unity Function(s)
+  //--------------------------------------------------------------
+  private void Awake()
+  {
+    Init();
+  }
+
+  //--------------------------------------------------------------
+  // Init(s)
+  //--------------------------------------------------------------
+  void Init()
+  {
+    int totalTile = (int)(_noOfTile.x * _noOfTile.y);
+    GameObject go = null;
+    float x = 0;
+    float y = 0;
+    Vector3 offsetToCenter = new Vector3((_noOfTile.x - 1) * _tileSize.x / 2, (_noOfTile.y - 1) * _tileSize.y / 2, 0);
+
+    for (int i = 0; i < totalTile; ++i)
     {
-        Init();
+      //--------------------------------------
+      // Spawn Tile
+      //--------------------------------------
+      go = Instantiate(Prefab_Tile, new Vector3(x * _tileSize.x - offsetToCenter.x, y * _tileSize.y - offsetToCenter.y, 0), Quaternion.identity);
+      go.transform.localScale = new Vector3(_tileSize.x, _tileSize.y, 0);
+
+      //--------------------------------------
+      // Increment X and Y for next tile
+      //--------------------------------------
+      ++x;
+
+      // If reached end of row, 
+      if (x % _noOfTile.x == 0)
+      {
+        ++y;
+        x = 0;
+      }
+
+
+      //--------------------------------------
+      // Adjust Script
+      //--------------------------------------
+      Tile tile = go.GetComponent<Tile>();
+      if (tile)
+      {
+        tile.SetAll(TileType.EMPTY, i, 0, _tileSize);
+        _listOfTile.Add(tile);
+      }
     }
 
-    //--------------------------------------------------------------
-    // Init(s)
-    //--------------------------------------------------------------
-    void Init()
+
+    // Manual Change TileType to obstacle
+    MakeBorderObstacle();
+    //MakeMiddleObstacle();
+    //MakeMiddleLeaveGap(2);
+    MakeStartAndGoalAuto();
+
+    CreateHeatMapGeneration();
+  }
+
+  //--------------------------------------------------------------
+  // Tile Plotting(s)
+  //--------------------------------------------------------------
+  void MakeBorderObstacle()
+  {
+    for (int i = 0; i < _listOfTile.Count; ++i)
     {
-        int totalTile = (int)(_noOfTile.x * _noOfTile.y);
-        GameObject go = null;
-        float x = 0;
-        float y = 0;
-        Vector3 offsetToCenter = new Vector3((_noOfTile.x-1) * _tileSize.x / 2, (_noOfTile.y-1) * _tileSize.y / 2, 0);
+      if (
+          (i % _noOfTile.x == 0) ||                       // First Column
+          (i % _noOfTile.x == _noOfTile.x - 1) ||         // Last  Column
+          ((int)(i / _noOfTile.x) == 0) ||                // First Row
+          ((int)(i / _noOfTile.x) == _noOfTile.y - 1)     // Last  Row
+        )
+      {
+        _listOfTile[i].SetType(TileType.OBSTACLE);
+        _listOfTile[i].SetDCost(-1);
+      }
+    }
+  }
 
-        for(int i = 0; i < totalTile; ++i)
+  void MakeMiddleObstacle()
+  {
+    int middle = (int)(_noOfTile.x / 2);
+
+    for (int i = 0; i < _listOfTile.Count; ++i)
+    {
+      if (i % _noOfTile.x == middle)
+      {
+        _listOfTile[i].SetType(TileType.OBSTACLE);
+        _listOfTile[i].SetDCost(-1);
+      }
+    }
+  }
+
+  void MakeMiddleLeaveGap(int gap)
+  {
+    int middle = (int)(_noOfTile.x / 2);
+
+    for (int i = 0; i < _listOfTile.Count; ++i)
+    {
+      // If at border, skip
+      if (
+          ((int)(i / _noOfTile.x) == 0) ||                // First Row
+          ((int)(i / _noOfTile.x) == _noOfTile.y - 1)     // Last  Row
+        )
+      {
+        continue;
+      }
+
+
+      // If at middle, we see whether we can make a gap
+      if (i % _noOfTile.x == middle)
+      {
+        if (gap > 0)
         {
-            //--------------------------------------
-            // Spawn Tile
-            //--------------------------------------
-            go = Instantiate(Prefab_Tile, new Vector3(x * _tileSize.x - offsetToCenter.x, y * _tileSize.y - offsetToCenter.y, 0), Quaternion.identity);
-            go.transform.localScale = new Vector3(_tileSize.x, _tileSize.y, 0);
+          --gap;
+          _listOfTile[i].SetType(TileType.EMPTY);
+          _listOfTile[i].SetDCost(0);
+        }
+        else
+        {
+          _listOfTile[i].SetType(TileType.OBSTACLE);
+          _listOfTile[i].SetDCost(-1);
+        }
+      }
+    }
+  }
 
-            //--------------------------------------
-            // Increment X and Y for next tile
-            //--------------------------------------
-            ++x;
+  void MakeStartAndGoalAuto()
+  {
+    List<Tile> listEmptyTile = new List<Tile>();
 
-            // If reached end of row, 
-            if (x % _noOfTile.x == 0)
-            {
-                ++y;
-                x = 0;
-            }
+    for (int i = 0; i < _listOfTile.Count; ++i)
+    {
+      if (_listOfTile[i].GetTileType() == 0)
+      {
+        listEmptyTile.Add(_listOfTile[i]);
+      }
+    }
+
+    int randomIndex = Random.Range(0, listEmptyTile.Count);
+    _tileStart = listEmptyTile[randomIndex];
+    _tileStart.SetType(TileType.START);
+    listEmptyTile.RemoveAt(randomIndex);
+
+    randomIndex = Random.Range(0, listEmptyTile.Count);
+    _tileGoal = _listOfTile[_listOfTile.Count/2];//listEmptyTile[randomIndex];
+    _tileGoal.SetType(TileType.GOAL);
+    listEmptyTile.RemoveAt(randomIndex);
+
+  }
+
+  //--------------------------------------------------------------
+  // Algorithm
+  //--------------------------------------------------------------
+  void CreateHeatMapGeneration()
+  {
+    _openList = new List<Tile>();
+    _visitedList = new List<Tile>();
+    int i = 0;
+    Tile tempTile = null;
+
+    //-- Add first tile in first
+    _openList.Add(_tileGoal);
+    _visitedList.Add(_tileGoal);
+
+    //-- Loop until nothing left in openList
+    while (_openList.Count > 0)
+    {
+      // Get next openlist cell
+      tempTile = _openList[0];
+      _visitedList.Add(tempTile);
+
+      // Check left/right/top/bottom of current Node, if not visited add to openList
+      Find_Available_Neighbour_Nodes(tempTile.GetDCost(), tempTile.GetListID());
+
+      // Remove current node from openlist
+      _openList.Remove(tempTile);
+    }
+  }
+  void Find_Available_Neighbour_Nodes(int prev_dCost, int listID)
+  {
+    int x = (int)(listID % _noOfTile.x);
+    int y = (int)(listID / _noOfTile.x);
+    int newID = 0;
+    int tempSize = 0;
+    bool visited = false;
 
 
-            //--------------------------------------
-            // Adjust Script
-            //--------------------------------------
-            Tile tile = go.GetComponent<Tile>();
-            if(tile)
-            {
-                tile.SetAll(TileType.EMPTY, i, 0, _tileSize);
-                _listOfTile.Add(tile);
-            }
+    for (int xx = -1; xx <= 1; ++xx)
+    {
+      for (int yy = -1; yy <= 1; ++yy)
+      {
+        // Don't check, out of boundary or myself
+        if (
+            (xx == 0 && yy == 0) ||                     // Myself
+            (x + xx < 0 || x + xx > _noOfTile.x - 1) || // x boundary
+            (y + yy < 0 || y + yy > _noOfTile.y - 1)    // y boundary
+          )
+        {
+          continue;
         }
 
+        // Convert the next check position to listIndex
+        newID = ConvertArrayToListIndex(x + xx, y + yy);
 
-        // Manual Change TileType to obstacle
-        MakeBorderObstacle();
-        MakeMiddleObstacle();
-        MakeMiddleLeaveGap(2);
-        MakeStartAndGoalAuto();
-    }
+        // Check if is obstacle, we continue;
+        if (_listOfTile[newID].GetTileType() == TileType.OBSTACLE)
+          continue;
 
-    //--------------------------------------------------------------
-    // Tile Plotting(s)
-    //--------------------------------------------------------------
-    void MakeBorderObstacle()
-    {
-        for(int i = 0; i < _listOfTile.Count; ++i)
+        // Check if visited
+        tempSize = _visitedList.Count;
+        visited = false;
+        for(int i = 0; i < tempSize; ++i)
         {
-            if (
-                (i % _noOfTile.x == 0) ||                       // First Column
-                (i % _noOfTile.x == _noOfTile.x - 1) ||         // Last  Column
-                ((int)(i / _noOfTile.x) == 0) ||                // First Row
-                ((int)(i / _noOfTile.x) == _noOfTile.y - 1)     // Last  Row
-              )
-            {
-                _listOfTile[i].SetType(TileType.OBSTACLE);
-                _listOfTile[i].SetDCost(-1);
-            }
+          if(_visitedList[i].GetListID() == newID)
+          {
+            visited = true;
+            break;
+          }
         }
-    }
+        Debug.Log("Current: " + listID + ", Check if " + newID + "is visited? : " + visited);
 
-    void MakeMiddleObstacle()
-    {
-        int middle = (int)(_noOfTile.x / 2);
-
-        for (int i = 0; i < _listOfTile.Count; ++i)
+        // If not visited, add this to openlist
+        if (!visited)
         {
-            if (i % _noOfTile.x == middle)
-            {
-                _listOfTile[i].SetType(TileType.OBSTACLE);
-                _listOfTile[i].SetDCost(-1);
-            }
+          _listOfTile[newID].SetDCost(prev_dCost + 1);
+          _openList.Add(_listOfTile[newID]);
+          _visitedList.Add(_listOfTile[newID]);
         }
+      }
     }
-
-    void MakeMiddleLeaveGap(int gap)
-    {
-        int middle = (int)(_noOfTile.x / 2);
-
-        for (int i = 0; i < _listOfTile.Count; ++i)
-        {
-            // If at border, skip
-            if(
-                ((int)(i / _noOfTile.x) == 0) ||                // First Row
-                ((int)(i / _noOfTile.x) == _noOfTile.y - 1)     // Last  Row
-              )
-            {
-                continue;
-            }
+  }
 
 
-            // If at middle, we see whether we can make a gap
-            if (i % _noOfTile.x == middle)
-            {
-                if (gap > 0)
-                {   
-                    --gap;
-                    _listOfTile[i].SetType(TileType.EMPTY);
-                    _listOfTile[i].SetDCost(0);
-                }
-                else
-                {
-                    _listOfTile[i].SetType(TileType.OBSTACLE);
-                    _listOfTile[i].SetDCost(-1);
-                }
-            }
-        }
-    }
+  //--------------------------------------------------------------
+  // Converter
+  //--------------------------------------------------------------
+  Vector2 ConvertListToArrayCoordinate(int listID)
+  {
+    return new Vector2((int)(listID % _noOfTile.x), (int)(listID / _noOfTile.x));
+  }
+  int ConvertArrayToListIndex(int x, int y)
+  {
+    return (int)(y * _noOfTile.x + x);
+  }
 
-    void MakeStartAndGoalAuto()
-    {
-        List<Tile> listEmptyTile = new List<Tile>();
 
-        for(int i = 0; i < _listOfTile.Count; ++i)
-        {
-            if(_listOfTile[i].GetTileType() == 0)
-            {
-                listEmptyTile.Add(_listOfTile[i]);
-            }
-        }
+  //--------------------------------------------------------------
+  // Getter(s)
+  //--------------------------------------------------------------
 
-        int randomIndex = Random.Range(0, listEmptyTile.Count);
-        _tileStart = listEmptyTile[randomIndex];
-        _tileStart.SetType(TileType.START);
-        listEmptyTile.RemoveAt(randomIndex);
-
-        randomIndex = Random.Range(0, listEmptyTile.Count);
-        _tileGoal = listEmptyTile[randomIndex];
-        _tileGoal.SetType(TileType.GOAL);
-        listEmptyTile.RemoveAt(randomIndex);
-
-    }
-
-    //--------------------------------------------------------------
-    // Algorithm
-    //--------------------------------------------------------------
-    void CreateHeatMapGeneration()
-    {
-        List<Tile> openList = new List<Tile>();
-        int i = 0;
-        Tile tempTile = null;
-
-        //-- Add first index in first
-        openList.Add(_tileGoal);
-
-        while(openList.Count > 0)
-        {
-            // Get 1 of the current node from openlist
-            tempTile = openList[openList.Count - 1];
-
-            // Check left of current Node
-            Find_Available_Neighbour_Nodes(tempTile.GetListID());
-
-            // Remove current node from openlist
-            openList.Remove(tempTile);
-        }
-    }
-    void Find_Available_Neighbour_Nodes(int listID)
-    {
-    }
-    
 }
--- 2D Dungeon Crawler/Assets/Scripts/Tile.cs
+++ 2D Dungeon Crawler/Assets/Scripts/Tile.cs
@@ -6,105 +6,109 @@ using UnityEditor;
 
 public enum TileType
 {
-    EMPTY,
-    OBSTACLE,
-    START,
-    GOAL,
-    TOTAL
+  EMPTY,
+  OBSTACLE,
+  START,
+  GOAL,
+  TOTAL
 }
 
 public class Tile : MonoBehaviour
 {
-    // Private
-    TileType        _type;                                  // What is the Tile Type?
-    int             _listID;                                // What is it's index in the list?        
-    int             _dCost;                                 // What is the d cost?
-    bool            _visited;                               // Have this tile been visited?
+  // Private
+  TileType _type;                                  // What is the Tile Type?
+  int _listID;                                // What is it's index in the list?        
+  int _dCost;                                 // What is the d cost?
+  bool _visited;                               // Have this tile been visited?
 
-    // Cache References
-    SpriteRenderer  _sr;
+  // Cache References
+  SpriteRenderer _sr;
 
-    // Gizmo
-    private Vector3 _gizmo_Offset_DCost;                    // What is offset for gizmo dCost?
-    private Color   _gizmo_Color_DCost = Color.black;       // What is color for gizmo dCost?
-    private GUIStyle _gizmo_Style_DCost = new GUIStyle();
-
-
-    //--------------------------------------------------------------
-    // Unity(s)
-    //--------------------------------------------------------------
-    private void Awake()
-    {
-        _sr = GetComponent<SpriteRenderer>();
-    }
-    private void OnDrawGizmos()
-    {
-        Handles.Label(transform.position + _gizmo_Offset_DCost, _dCost.ToString(), _gizmo_Style_DCost);
-    }
-
-    //--------------------------------------------------------------
-    // Setter(s)
-    //--------------------------------------------------------------
-    public void SetAll(TileType type, int listID, int dCost, Vector2 tileSize)
-    {
-        //-- Tile Data
-        _type = type;
-        _listID = listID;
-        _sr.color = ChooseTileColor(type);
-        _dCost = dCost;
-
-        //-- Gizmo Data
-        _gizmo_Offset_DCost = new Vector3(tileSize.x / 8, tileSize.y / 3, 0);
-        _gizmo_Style_DCost.normal.textColor = _gizmo_Color_DCost;
-        _gizmo_Style_DCost.fontSize = (int)(tileSize.x / 3);
-
-    }
-    public void SetType(TileType type)
-    {
-        _type = type;
-        _sr.color = ChooseTileColor(type);
-    }
-    public void SetDCost(int dCost)
-    {
-        _dCost = dCost;
-    }
-    public void SetVisited(bool flag)
-    {
-        _visited = flag;
-    }
+  // Gizmo
+  private Vector3 _gizmo_Offset_ListID;                   // What is offset for gizmo listID?
+  private Vector3 _gizmo_Offset_DCost;                    // What is offset for gizmo dCost?
+  private Color _gizmo_Color_DCost = Color.black;       // What is color for gizmo dCost?
+  private GUIStyle _gizmo_Style_DCost = new GUIStyle();
 
 
-    //--------------------------------------------------------------
-    // Getter(s)
-    //--------------------------------------------------------------
-    public TileType GetTileType()
-    {
-        return _type;
-    }
+  //--------------------------------------------------------------
+  // Unity(s)
+  //--------------------------------------------------------------
+  private void Awake()
+  {
+    _sr = GetComponent<SpriteRenderer>();
+  }
+  private void OnDrawGizmos()
+  {
+    Handles.Label(transform.position + _gizmo_Offset_DCost, _dCost.ToString(), _gizmo_Style_DCost);
+    Handles.Label(transform.position + _gizmo_Offset_ListID, _listID.ToString(), _gizmo_Style_DCost);
+  }
 
-    public int GetDCost()
-    {
-        return _dCost;
-    }
-    public int GetListID()
-    {
-        return _listID;
-    }
-    public bool GetVisited()
-    {
-        return _visited;
-    }
+  //--------------------------------------------------------------
+  // Setter(s)
+  //--------------------------------------------------------------
+  public void SetAll(TileType type, int listID, int dCost, Vector2 tileSize)
+  {
+    //-- Tile Data
+    _type = type;
+    _listID = listID;
+    _sr.color = ChooseTileColor(type);
+    _dCost = dCost;
 
-    //--------------------------------------------------------------
-    // Shortcut(s)
-    //--------------------------------------------------------------
-    Color ChooseTileColor(TileType type)
-    {
-        if (type == TileType.EMPTY) return Color.white;
-        else if (type == TileType.OBSTACLE) return Color.yellow;
-        else if (type == TileType.START) return Color.blue;
-        else if (type == TileType.GOAL) return Color.red;
+    //-- Gizmo Data
+    _gizmo_Offset_DCost = new Vector3(tileSize.x / 8, tileSize.y / 3, 0);
+    _gizmo_Style_DCost.normal.textColor = _gizmo_Color_DCost;
+    _gizmo_Style_DCost.fontSize = (int)(tileSize.x / 6);
 
-        return Color.black;
-    }
+    _gizmo_Offset_ListID = new Vector3(-20, 0, 0);
+
+  }
+  public void SetType(TileType type)
+  {
+    _type = type;
+    _sr.color = ChooseTileColor(type);
+  }
+  public void SetDCost(int dCost)
+  {
+    _dCost = dCost;
+  }
+  public void SetVisited(bool flag)
+  {
+    _visited = flag;
+  }
+
+
+  //--------------------------------------------------------------
+  // Getter(s)
+  //--------------------------------------------------------------
+  public TileType GetTileType()
+  {
+    return _type;
+  }
+
+  public int GetDCost()
+  {
+    return _dCost;
+  }
+  public int GetListID()
+  {
+    return _listID;
+  }
+  public bool GetVisited()
+  {
+    return _visited;
+  }
+
+  //--------------------------------------------------------------
+  // Shortcut(s)
+  //--------------------------------------------------------------
+  Color ChooseTileColor(TileType type)
+  {
+    if (type == TileType.EMPTY) return Color.white;
+    else if (type == TileType.OBSTACLE) return Color.yellow;
+    else if (type == TileType.START) return Color.blue;
+    else if (type == TileType.GOAL) return Color.red;
+
+    return Color.black;
+  }
 }
